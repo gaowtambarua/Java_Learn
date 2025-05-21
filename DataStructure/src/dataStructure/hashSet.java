bf79cca10aab02635c1d8fd0948a7291eb665502
Gaowtam Barua <128220812+gaowtambarua@users.noreply.github.com>
2025-05-21 21:24:23 +0600

    update

--- DataStructure/src/dataStructure/hashSet.java
+++ DataStructure/src/dataStructure/hashSet.java
@@ -5,15 +5,15 @@ import java.util.HashSet;
 public class hashSet {
 	public hashSet()
 	{
-		int[] arr1 = {2,0,10,-19,4,6,-8};
+		int[] arr1 = {7,1,14,11};
 		System.out.println(checkIfExistJava(arr1)); 
 	}
-	//////////////////////////////////////////Java basic code 1346 code	
+	//////////////////////////////////////////Java basic code leadcode problem 1346 code	
 	public boolean checkIfExistJava(int[] arr) {
 		HashSet<Integer> seen=new HashSet<Integer>();
 		for(int num:arr)
 		{
-			if(seen.contains(num*2)||(num % 2 == 0 && seen.contains(num / 2)))
+			if(seen.contains(num*2)||(num%2==0&&seen.contains(num/2)))
 			{
 				return true;
 			}
