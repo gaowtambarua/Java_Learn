e728bd1719cc5fea11d08a9972c4bd8f8669f6c1
Gaowtam Barua <128220812+gaowtambarua@users.noreply.github.com>
2026-02-17 03:59:08 +0600

    update

--- DataStructure/src/dataStructure/StackExample.java
+++ DataStructure/src/dataStructure/StackExample.java
@@ -1,6 +1,23 @@
 package dataStructure;
 import java.util.Stack;
 public class StackExample {
+	//	public StackExample()
+	//	{
+	////		Stack<Integer> stack = new Stack<Integer>();
+	////
+	////		//////////////Last In fast Out
+	////
+	////		stack.push(10);  // ঢুকানো
+	////		stack.push(20);
+	////		stack.push(30);
+	////
+	////		System.out.println(stack.pop()); // বের করে → 30
+	////		System.out.println(stack.peek()); // দেখে → 20 (কিন্তু বের করে না)
+	////		System.out.println(stack.pop());
+	////		System.out.println(stack.peek());
+	////		System.out.println(stack.pop());
+	//	}
+
 	public StackExample()
 	{
 		Stack<Integer> stack = new Stack<Integer>();
