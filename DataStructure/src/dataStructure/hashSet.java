ad95e97744ccabd87cc96fbae85461d041446b62
Gaowtam Barua <128220812+gaowtambarua@users.noreply.github.com>
2026-03-26 15:25:41 +0600

    update

--- DataStructure/src/dataStructure/hashSet.java
+++ DataStructure/src/dataStructure/hashSet.java
@@ -1,6 +1,6 @@
 package dataStructure;
 
-import java.util.HashSet;
+import java.util.*;
 
 public class hashSet {
 	public hashSet()
@@ -17,6 +17,37 @@ public class hashSet {
 		
 		int[] arr1 = {7,1,14,11};
 		System.out.println(checkIfExistJava(arr1)); 
+		
+		
+		
+		
+		////////////////////////////////////////////////////////////////////////////
+		Set<Integer> set = new TreeSet<>(Collections.reverseOrder());
+
+		set.add(5);
+		set.add(1);
+		set.add(3);
+
+		System.out.println(set);
+		
+		
+		
+		//////////////////////////////////////////////////
+		
+		TreeSet<Integer> set2 = new TreeSet<>();
+
+		set2.add(5);
+		set2.add(1);
+		set2.add(3);
+		set2.add(2);
+
+		set2.pollFirst();
+
+		System.out.println(set2);
+		
+		
+		
+		
 	}
 	//////////////////////////////////////////Java basic code leadcode problem 1346 code	
 	public boolean checkIfExistJava(int[] arr) {
