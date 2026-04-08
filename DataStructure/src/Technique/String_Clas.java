0931c8d0ebb24a8755a7c2a34cfac34899f97d7a
Gaowtam Barua <128220812+gaowtambarua@users.noreply.github.com>
2026-04-08 19:24:20 +0600

    update

---
+++ DataStructure/src/Technique/String_Clas.java
@@ -0,0 +1,14 @@
+package Technique;
+
+public class String_Clas {
+	public String_Clas()
+	{
+		String s = "   Aka   ";
+
+//		System.out.println(s.stripTrailing()); // "   Aka"
+//		System.out.println(s.stripLeading());  // "Aka   "
+//		System.out.println(s.strip());         // "Aka"  
+		
+		///////////////// not spported update javas
+	}
+}
